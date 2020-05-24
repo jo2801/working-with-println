ca1b8fb604b6433910a7f1cf6cd965b6263bfed9
jo2801 <65859102+jo2801@users.noreply.github.com>
2020-05-24 16:33:11 +0200

    added the code ^^

---
+++ thecode.java
@@ -0,0 +1,15 @@
+public class Aufgabe4 {
+	public static void main(String[] args){
+		int a = 20;
+		int b = 3;
+		int plus = a+b;
+		int minus = a-b;
+		int mal = a*b;
+		int durch = a/b;
+
+		System.out.println("Addition:"+a+"+"+b+"="+plus);
+		System.out.println("Subtraktion:"+a+"-"+b+"="+minus);
+		System.out.println("Multiplikation:"+a+"x"+b+"="+mal);
+		System.out.println("Division:"+a+":"+b+"="+durch);
+	}
+}
